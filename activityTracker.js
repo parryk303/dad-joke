b8b83d1a03cd9de9ad2fc912c81f761961582111
parryk303 <kyle.parry.303@gmail.com>
2022-07-17 16:22:07 -0600

    Update jokes

--- activityTracker.js
+++ activityTracker.js
@@ -1,1 +1,2 @@
 {"success":true,"body":[{"_id":"60dd36bcd66e1a566c6ad0d6","setup":"Who are the worlds fastest readers","punchline":"The 911 jumpers, one hundred stories in a few seconds","type":"jumper","likes":[],"author":{"name":"unknown","id":null},"approved":true,"date":1618108661,"NSFW":false}]},
+{"success":true,"body":[{"_id":"60dd37546e84962917fb6369","setup":"What do you call a guy with 5 rabbits in his ass?","punchline":"Warren.","type":"rabbit","likes":[],"author":{"name":"unknown","id":null},"approved":true,"date":1618108661,"NSFW":false}]},
