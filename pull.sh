41dca58d7c6d25a943d0d0879b4381586987a061
parryk303 <kyle.parry.303@gmail.com>
2023-02-06 13:03:38 -0700

    Update bash for aws

--- pull.sh
+++ pull.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-loc=echo pwd
+cd /home/ec2-user/dad-joke
 git pull origin main
--- push.sh
+++ push.sh
@@ -2,12 +2,11 @@
 
 # put current date as yyyy-mm-dd in $date
 date=$(date '+%Y-%m-%d')
-
 message="Daily Joke for ${date}"
+
 cd /Users/kyle/Desktop/dad-joke
+
 git add .
 git commit -m "${message}"
 git push origin main
-
-loc=echo pwd
 say 'Hey Kyle I am updating jokes'
